4c6d96616e0d176204ced81ae7fc585b081b3892
Kyle Davis <validkeys@gmail.com>
2022-03-21 08:33:14 -0400

    updated to work with various capabilities

--- addon/services/did-resize-detector.js
+++ addon/services/did-resize-detector.js
@@ -1,19 +1,19 @@
 import Service from '@ember/service';
 import elementResizeDetectorMaker from 'element-resize-detector';
 
-export default Service.extend({
-  init() {
-    this._super(...arguments);
+export default class DidResizeService extends Service {
+  constructor() {
+    super(...arguments);
     this.detector = elementResizeDetectorMaker({
-      strategy: "scroll"
+      strategy: 'scroll',
     });
-  },
+  }
 
   setup(element, callback, options) {
     this.detector.listenTo(options, element, callback);
-  },
+  }
 
   teardown(element, callback) {
     this.detector.removeListener(element, callback);
   }
-});
+}
